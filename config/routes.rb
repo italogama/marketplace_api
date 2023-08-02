5b524bff61c1d750b0abb097786f98668d80387b
Italo Gama <italo.gama@findup.com.br>
2023-08-01 23:25:30 -0300

    :sparkles: Remove comments/ add namespace
    remove comments and add namespace to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@
 Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+
+  end
+
 end
